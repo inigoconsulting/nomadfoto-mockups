9e861633ed028ec6d547013bcb69e3d5799b3360
Maverick JS <mavjs01@gmail.com>
2012-12-20 12:51:27 +0800

    tidy up and put unneeded stuff to extra.js

--- js/collection.js
+++ js/collection.js
@@ -1,24 +1,5 @@
         (function($) {
-            var basket_count = 0;
-
-            var TEMPLATES = {
-        
-            'basket': $(
-                '<div class="basket">'+ //<div class="dropzone well">' +
-                '<div class="basket-form">' + 
-                '<div class="form">' +
-                '<div class="control-group">' +
-                '<label class="control-label" for="basket-action">Action</label>' + 
-                '<div class="controls">' + 
-                '<select class="basket-action">' +
-                '<option value="print">Print</option><option value="edit">Edit</option>' +
-                '</select>' + 
-                '</div></div></div>' + 
-                '<div class="basket-options-form"></div></div></div><br />'
-            )
-            }
                 var populate_gallery = function (node, images) {
-                var gallerylen = 0;
                 $.each(images, function (idx, image) {
                     node.append('<div class="item"><a href="#" id="boxeffect"><img src="' + image.url + '" /></a></div>');
                 })
@@ -40,12 +21,6 @@
                     });
                 });
                 $('#joborder').click(function (ev) {
-                    ev.preventDefault();
-                    var basket = TEMPLATES['basket'].clone();
-                    basket.attr('id', 'basket-' + basket_count);
-                    basket_count += 1;
-                    $('#baskets-container').append(basket);
-                    
                 });
             });
         })(jQuery);
