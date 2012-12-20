bc7b4c70dfee37ee6e1fe5ed5cd6059687f3b047
Maverick JS <mavjs01@gmail.com>
2012-12-20 12:26:18 +0800

    added auto updated job order form

--- js/collection.js
+++ js/collection.js
@@ -29,33 +29,23 @@
                     $('.thumbnails').sortable().disableSelection();
                     $('div.item:first').addClass('active');
                 });
+                var actionvar = $('.basket-action').val();
+                $.get('json/' + actionvar + 'form.html', function (data) {
+                    $('.basket-options-form').html(data);
+                });
+                $('.basket-action').change(function (ev) {
+                    var action = $(ev.target).val();
+                    $.get('json/' + action + 'form.html', function (data) {
+                        $('.basket-options-form').html(data);
+                    });
+                });
                 $('#joborder').click(function (ev) {
                     ev.preventDefault();
                     var basket = TEMPLATES['basket'].clone();
                     basket.attr('id', 'basket-' + basket_count);
                     basket_count += 1;
                     $('#baskets-container').append(basket);
-                    $('#thumbnails', basket).droppable({
-                        drop: function( event, ui ) {
-                        $('.drag-placeholder', basket).remove();
-                    $(this).append('<li>' + ui.draggable.html() + '</li>');
-                }
+                    
                 });
-                    $('.basket-action', basket).change(function (ev) {
-                        var action = $(ev.target).val();
-                        $.get('json/' + action + 'form.html', function (data) {
-                        $('.basket-options-form', basket).html(data);
-                        });
-                    });
-                    $('.basket-action', basket).trigger('change');
-                    if (! $('#submit-order').length ) {
-                        $('#add-basket').after(
-                            '<a href="#" id="submit-order" class="btn btn-success">Submit Order</a>'
-                    );
-                    $('#submit-order').click(function () {
-                        alert($('.gallery').offset().top);//'Not implemented yet');
-                    });
-                }
-            });
             });
         })(jQuery);
