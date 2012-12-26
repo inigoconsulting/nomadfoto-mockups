0dba4a27f8cf4b0d90dc8afd9cb6ed6a87edc421
Maverick JS <mavjs01@gmail.com>
2012-12-26 12:13:37 +0800

    more JS stuff

--- js/collection.js
+++ js/collection.js
@@ -1,26 +1,53 @@
-        (function($) {
-                var populate_gallery = function (node, images) {
-                $.each(images, function (idx, image) {
-                    node.append('<div class="item"><a href="#" id="boxeffect"><img src="' + image.url + '" /></a></div>');
-                })
-            }
-            $(document).ready(function() {
-                $.getJSON('json/images.json', {}, function (data) {
-                    populate_gallery($('.thumbnails'), data)
-                    $('.thumbnails').sortable().disableSelection();
-                    $('div.item:first').addClass('active');
-                });
-                var actionvar = $('.basket-action').val();
-                $.get('json/' + actionvar + 'form.html', function (data) {
-                    $('.basket-options-form').html(data);
-                });
-                $('.basket-action').change(function (ev) {
-                    var action = $(ev.target).val();
-                    $.get('json/' + action + 'form.html', function (data) {
-                        $('.basket-options-form').html(data);
-                    });
-                });
-                $('#joborder').click(function (ev) {
-                });
+(function ($) {
+    // function to append to a certain node in html
+    populate_gallery = function (node, images) {
+        $.each(images, function (idx, image) {
+            node.append('<li style="display: block;"><div><img src="' + image.url + '" /></div></li>');
+            //node.append('<div class="item"><a href="#" id="boxeffect"><img src="' + image.url + '" /></a></div>');
+        });
+    };
+    // function to GetForms.
+    GetForms = function () {
+        $.get('json/' + $('.basket-action').val() + 'form.html', function (data) {
+            $('.basket-options-form').html(data);
+        });
+    };
+    // function to ShowForms.
+    ShowForms = function () {
+        $('.basket-action').change(function (ev) {
+            var action = $(ev.target).val();
+            $.get('json/' + action + 'form.html', function (data) {
+                $('.basket-options-form').html(data);
             });
-        })(jQuery);
+        });
+    }
+    var jobs = function () {
+                var count = 0;
+                $.get('json/jobsection.html', function (data) {
+                    var startsec = '<section class="slide" id="jobs-'+count+'">\n';
+                    var endsec = '</section>\n';
+                    $('.JobSection').append(startsec + data + endsec);
+                    GetForms();
+                    ShowForms();
+                    $.getJSON('json/images.json', {}, function (data) {
+                          populate_gallery($('.thumbnails'), data)
+                          //$('div.item:first').addClass('active');
+                    });
+                $('#joborder').click(function() {
+                    jobs();
+                count = count + 1;
+                });
+              });
+            }
+     // on document ready load the functions
+     $(document).ready(function() {
+            jobs();
+            var mySlider = new Swipe(document.getElementById('slider'));
+            $('#prev').click(function () {
+                mySlider.prev();
+            });
+            $('#next').click(function () {
+                mySlider.next();
+            });
+     });
+})(jQuery);
